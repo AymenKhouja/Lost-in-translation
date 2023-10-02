f212e5c85f1556170b971e2979fc446f399b9ab1
Aymen Khouja <98080901+AymenKhouja@users.noreply.github.com>
2023-10-02 08:07:43 +0100

    Update LostInTranslation.py

--- LostInTranslation.py
+++ LostInTranslation.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Sep  7 19:36:46 2023
 
-@author: aymen
-"""
 from ImageGeneration import generate_image
 from GameLogic import *
 from ServerLogic import *
